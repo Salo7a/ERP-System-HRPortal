7551a2d5a19be6c9ac39e880b4f485ef7a45b48b
Salo7a <salo7a98x@gmail.com>
2021-08-27 00:33:51 +0200

    Directorate Updates

--- EnactusPortal/routes/admin.js
+++ EnactusPortal/routes/admin.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const {User, Invite, Team, Directorate} = require('../models');
-const {NotAuth, isAuth} = require('../utils/filters');
+const {NotAuth, isAuth, isAdmin} = require('../utils/filters');
 const {check, validationResult, body} = require('express-validator');
 const {Op} = require('sequelize');
 const Chance = require('chance');
 const createError = require("http-errors");
 
-
-router.get("/users", isAuth, (req, res, next) => {
+// Users List
+router.get("/users", isAdmin, (req, res, next) => {
     if (!req.user.isAdmin) {
         next(createError(403))
     }
@@ -23,8 +23,8 @@ router.get("/users", isAuth, (req, res, next) => {
     )
 });
 
-
-router.post('/delete', isAuth, (req, res, next) => {
+// Delete User
+router.post('/delete', isAdmin, (req, res, next) => {
     if (!req.user.isAdmin) {
         next(createError(403))
     }
@@ -36,7 +36,8 @@ router.post('/delete', isAuth, (req, res, next) => {
     res.send({id: req.body.UserId, msg: `#${req.body.UserId} Deleted Successfully`});
 });
 
-router.get("/directorates", async (req, res, next) => {
+// All Directorates
+router.get("/directorates", isAdmin, async (req, res, next) => {
     if (!req.user.isAdmin) {
         next(createError(403))
     }
@@ -44,7 +45,8 @@ router.get("/directorates", async (req, res, next) => {
     res.render('admin/directoratesView', {title: "All Directorates", directorates})
 })
 
-router.get("/directorate/edit", async (req, res, next) => {
+// Directorate Edit Form
+router.get("/directorate/edit", isAdmin, async (req, res, next) => {
     if (!req.user.isAdmin) {
         next(createError(403))
     }
@@ -57,11 +59,18 @@ router.get("/directorate/edit", async (req, res, next) => {
     res.render('admin/directorateEdit', {directorate})
 })
 
-router.post("/directorate/edit", async (req, res, next) => {
-    next()
+// Edit Directorate Info
+router.post("/directorate/edit", isAdmin, async (req, res, next) => {
+    let {name, id} = req.body
+    Directorate.findOne({where: {id}}).then((dir)=>{
+        dir.Name = Name
+        dir.save()
+        res.send({msg: "Updated Successfully"})
+    })
 })
 
-router.post("/directorates/add", async (req, res, next) => {
+// Add New Directorate
+router.post("/directorates/add", isAdmin, async (req, res, next) => {
     if (!req.user.isAdmin) {
         next(createError(403))
     }
@@ -73,7 +82,7 @@ router.post("/directorates/add", async (req, res, next) => {
     }))
 })
 
-router.get("/teams", async (req, res, next) => {
+router.get("/teams", isAdmin, async (req, res, next) => {
     if (!req.user.isAdmin) {
         next(createError(403))
     }

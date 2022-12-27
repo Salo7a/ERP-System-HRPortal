5b67683c4092869e092ed1e87ad61c2a7020adc5
Salo7a <salo7a98x@gmail.com>
2022-12-27 23:32:32 +0200

    Define DB Connection Settings

--- config/config.js
+++ config/config.js
@@ -19,14 +19,20 @@ module.exports = {
   },
   "production": {
     "username": process.env.PROD_DB_USERNAME || "root",
-    "password": process.env.PROD_DB_PASSWORD || "",
+    "password": process.env.PROD_DB_PASSWORD || "mysql",
     "database": process.env.PROD_DB_NAME || "portalprod",
     "host": process.env.PROD_DB_HOST || "127.0.0.1",
     "dialect": process.env.PROD_DB_DIALECT || "mysql",
     "dialectOptions": {
       "timezone": "local"
     },
-    "timezone": process.env.PROD_DB_TMZ || "Africa/Cairo"
+    "timezone": process.env.PROD_DB_TMZ || "Africa/Cairo",
+    "pool": {
+      "max": 5,
+      "min": 0,
+      "acquire": 60000,
+      "idle": 10000
+    }
   }
 }
 
